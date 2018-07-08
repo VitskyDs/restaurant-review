23299dc73639220cc30c1edbcb092a98187296b4
Vitsky <vitskyds@gmail.com>
2018-07-08 20:57:25 +0300

    cleaned up lazy loading

--- js/main.js
+++ js/main.js
@@ -72,22 +72,22 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
  * Initialize leaflet map, called from HTML.
  */
 initMap = () => {
-        self.newMap = L.map('map', {
-            center: [40.722216, -73.987501],
-            zoom: 12,
-            scrollWheelZoom: false
-        });
-        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-            mapboxToken: 'pk.eyJ1Ijoidml0c2t5ZHMiLCJhIjoiOFZwQU50MCJ9.VyMMFpXAN7S8S1M0ryW1mQ',
-            maxZoom: 18,
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-                '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-                'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-            id: 'mapbox.streets'
-        }).addTo(newMap);
+    self.newMap = L.map('map', {
+        center: [40.722216, -73.987501],
+        zoom: 12,
+        scrollWheelZoom: false
+    });
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+        mapboxToken: 'pk.eyJ1Ijoidml0c2t5ZHMiLCJhIjoiOFZwQU50MCJ9.VyMMFpXAN7S8S1M0ryW1mQ',
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'
+    }).addTo(newMap);
 
-        updateRestaurants();
-    }
+    updateRestaurants();
+}
 
 /**
  * Update page and map for current restaurants.
@@ -148,7 +148,7 @@ createRestaurantHTML = (restaurant) => {
     li.setAttribute('role', 'listitem')
 
     const image = document.createElement('img');
-    image.className = 'restaurant-img';
+    image.classList.add('restaurant-img');
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
     image.setAttribute('alt', restaurant.name + 'restaurant, ' + restaurant.neighborhood);
 
